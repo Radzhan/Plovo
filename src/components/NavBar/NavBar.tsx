612b9a3cd6892af4809129a8dda7487575924318
Radjan Bayhombaev <rbayhombaev@gmail.com>
2022-12-25 08:44:47 +0600

    start project create components

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <div className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+                <span className="navbar-brand">Plovo</span>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
---
+++ src/components/Users/UsersItem.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UsersItem = () => {
+    return (
+        <div>
+            UserForm
+        </div>
+    )
+}
+
+export default UsersItem
